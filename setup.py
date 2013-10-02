b57e585d5cd8cabea90b6d5292ebe0e2989e0611
Daniel Craigmile <daniel.craigmile@mutualmobile.com>
2013-10-02 17:32:19 -0500

    trying to fix setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@
 
 from distutils.core import setup
 
-setup(name='Distutils',
+setup(name='django_crowd_authz',
       version='1.0',
       description='Crowd Authentication for Django',
       author='Daniel Craigmile',
       author_email='danielc@pobox.com',
       url='https://github.com/x110dc/django_crowd_authz',
       py_modules=['auth.backends'],
+      install_requires=['requests == 1.2.3'],
       )
