fc9d26a71c842fc2bcf5294be6b6442796497a6f
Daniel Craigmile <daniel.craigmile@mutualmobile.com>
2013-10-02 16:31:58 -0500

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='1.0',
+      description='Crowd Authentication for Django',
+      author='Daniel Craigmile',
+      author_email='danielc@pobox.com',
+      url='https://github.com/x110dc/django_crowd_authz',
+      py_modules=['auth.backends'],
+      )
